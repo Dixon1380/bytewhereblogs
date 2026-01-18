58773a1887b7996dfc62ea5c7a92b6bd6be3fb11
David Dixon <DixonDavid1332@gmail.com>
2026-01-18 17:44:53 +0000

    Modified site/astro.config.mjs

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -7,7 +7,8 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://bytewhere.com',
-  srcDir: './site/src', 
+  srcDir: './src',
   integrations: [mdx(), sitemap()],
 });
+
 
